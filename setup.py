b2d52c93187b05c075ced0204f8660f9bd966d2e
Jesue Junior <jesuesousa@gmail.com>
2020-03-17 21:07:17 -0300

    Infra files

--- setup.py
+++ setup.py
@@ -1,30 +1,38 @@
 from codecs import open  # To use a consistent encoding
 from os import path
 
-from setuptools import (  # Always prefer setuptools over distutils
-    find_packages,
-    setup,
-)
+from setuptools import find_packages, setup  # Always prefer setuptools over distutils
 
 here = path.abspath(path.dirname(__file__))
 
 setup(
-    name="django-mode",
+    name="modeanalytics",
     version="0.0.1",
     description="Django Mode Analytics Whitelabel",
-    long_description="",
-    url="https://github.com/jesuejunior/django-mode",
+    long_description=(open("README.md").read()),
+    url="https://github.com/jesuejunior/django-modeanalytics",
     author="jesue Junior",
     author_email="jesuesousa@gmail.com",
     license="MIT",
+    platforms=["OS Independent"],
+    keywords="mode analytics data dashboard bi",
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Natural Language :: Portuguese (Brazilian)",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
-    test_suite="tests",
-    install_requires=[],
+    packages=find_packages(exclude=["contrib", "docs", "tests*", "test_app", "test_project"]),
+    include_package_data=True,
+    test_suite="",
+    install_requires=["django", "psycopg2>=2"],
     entry_points={},
+    zip_safe=False,
 )
