72efeee85307c8f979b9fee06c895e7ca86ca233
Jesue Junior <jesuesousa@gmail.com>
2020-03-18 23:37:50 -0300

    Adding hotfix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name="modeanalytics",
-    version="0.1.0",
+    version="0.1.1",
     description="Django Mode Analytics Whitelabel",
     long_description=(open("README.md").read()),
     long_description_content_type="text/markdown",
